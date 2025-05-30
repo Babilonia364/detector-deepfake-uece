010f5a9612b5a2d00a6e2fb3d05cb1207a3ae86a
Armaan Tokas <armaantokas@gmail.com>
2025-05-30 08:23:31 +0530

    Update run_extraction.py

--- run_extraction.py
+++ run_extraction.py
@@ -1,15 +1,4 @@
-from extract_frames import extract_frames
-import os
-from tqdm import tqdm
+from extract_faces import extract_all_faces
 
-def process_all(input_folder, output_folder):
-    os.makedirs(output_folder, exist_ok=True)
-    for video_file in tqdm(os.listdir(input_folder)):
-        in_path = os.path.join(input_folder, video_file)
-        out_path = os.path.join(output_folder, video_file.split('.')[0])
-        extract_frames(in_path, out_path)
-
-process_all("data/train/real", "frames/train/real")
-process_all("data/train/fake", "frames/train/fake")
-process_all("data/test/real", "frames/test/real")
-process_all("data/test/fake", "frames/test/fake")
+extract_all_faces("Train Data Location", "Target Location (Train)")
+extract_all_faces("Test Data Location", "Target Location (Test)")
