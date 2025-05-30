a02adaa42c0a2b95e1b640fbc3692ec1d5fafd00
Armaan Tokas <armaantokas@gmail.com>
2025-05-30 08:52:24 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,24 +1,6 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms, models
 from sklearn.metrics import accuracy_score, roc_auc_score
 import numpy as np
-import os
-
-device = torch.device('cuda')
-
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize([0.5]*3, [0.5]*3)
-])
-
-data_dir = "Adress Faces Stored"
-train_data = datasets.ImageFolder(os.path.join(data_dir, "train"), transform=transform)
-test_data = datasets.ImageFolder(os.path.join(data_dir, "test"), transform=transform)
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
 def evaluate(model, loader):
     model.eval()
