626143891871fe3076cd3f5d2396bfe65fba18c1
Armaan Tokas <armaantokas@gmail.com>
2025-05-30 08:24:46 +0530

    Update resnet_classifier.py

--- resnet_classifier.py
+++ resnet_classifier.py
@@ -4,4 +4,5 @@ import torch.nn as nn
 def get_resnet18(num_classes=2):
     model = models.resnet18(pretrained=True)
     model.fc = nn.Linear(model.fc.in_features, num_classes)
+    model = model.to('cuda') #or cpur or preffered device
     return model
